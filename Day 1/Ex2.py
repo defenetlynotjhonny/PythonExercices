ec2b875e8e42bb33ec98bc68d4e828b500f638d7
defenetlynotjhonny <114114244+defenetlynotjhonny@users.noreply.github.com>
2023-11-22 23:57:03 +0100

    22/11/2023 23:55 Exercice2 done.

--- Day 1/Ex2.py
+++ Day 1/Ex2.py
@@ -9,3 +9,55 @@ Implement a login process where users enter the
     password has been modified.
 """
 
+def initialprompt():
+    username = input("Username: ")
+    password = input("Password:")
+    return [username, password]
+
+
+
+def login():
+    data = initialprompt()
+    usern = data[0]
+    pw = data[1]
+    done = True
+    loggedin = False
+    userchoice = -1
+    while done != False:
+        if loggedin == False or userchoice == 0:
+            print("Enter the username to log in:")
+            inputusern = input()
+            print("Enter the password")
+            inputpw = input()
+            loggedin = True
+        
+        
+        if usern != inputusern or pw != inputpw:
+            print("Wrong Password or Username, please try again")
+            userchoice = 0
+            
+        if usern == inputusern and pw == inputpw:
+            loggendin = True
+            print("Login successful. How would you like to proceed: \n")
+            print("Change Password-(1)\n")
+            print("Logout -(2)\n")
+            print("Exit - (3)")
+            userchoice = int(input("New Password: "))
+            
+        if userchoice == 3:
+            print("Exiting...")
+            return "Exited"
+        
+        elif userchoice == 2:
+            loggedin = False
+            
+        elif userchoice == 1 and loggedin == True:
+            print("What should the new password be?")
+            pw = input()
+        
+        
+        
+    
+    return True
+
+login()
